4d69dea829b732888ed6df981c9a4357950e0cfe
Jorge Enrique <jorgeenriqueantunes@gmail.com>
2026-03-15 16:54:38 -0300

    adição de cores nas logos

--- script.js
+++ script.js
@@ -234,3 +234,14 @@ My goal is to constantly evolve as a developer and build solutions that positive
   atualizarCertificado();
 });
 
+const elements = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver(entries => {
+entries.forEach(entry => {
+if(entry.isIntersecting){
+entry.target.classList.add("show");
+}
+});
+});
+
+elements.forEach(el => observer.observe(el));
